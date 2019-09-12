3d60c93ecb30caf21a6367caccac5668ddd10a4c
byte33 <mltullo1016@gmail.com>
2019-09-12 11:20:39 -0400

    Lecture 4

---
+++ Lecture/Lecture4.c
@@ -0,0 +1,15 @@
+//Matteo Tullo
+//tullom
+//400175089
+
+#include <stdio.h>
+
+int main(void) {
+
+    char c = 'A';
+    int i = 10;
+    printf("value of c is %c and the size is %lu\n",c,sizeof(c));
+    printf("value of i is %d and the size is %lu\n",i,sizeof(i));
+
+    return 0;
+}
